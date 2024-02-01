0a39c6e7ff50ca4765cf83d811e34eba07925141
javiidoce <nix3odz@gmail.com>
2024-02-01 20:00:57 +0100

    ch

--- solitario (enunciado)/js/solitario.js
+++ solitario (enunciado)/js/solitario.js
@@ -218,3 +218,17 @@ function set_contador(contador, valor) {
 // Desarrollo de la continuación del juego
 // Funciones drag & drop
 /*** !!!!!!!!!!!!!!!!!!! CÓDIGO !!!!!!!!!!!!!!!!!!!! **/
+
+function dragStart(event) {
+	event.dataTransfer.setData("Text", event.target.id);
+  }
+
+  function allowDrop(event) {
+	event.preventDefault();
+  }
+
+  function drop(event) {
+	event.preventDefault();
+	var data = event.dataTransfer.getData("Text");
+	event.target.appendChild(document.getElementById(data));
+  }
